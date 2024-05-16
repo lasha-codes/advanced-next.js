f910b58dad3f2e40833cd4b7d05accc0dbfbf21a
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-16 15:25:48 -0700

    some auth working and toggle functionality this is so boring i wanna sleep

--- src/components/navbar/links/links.jsx
+++ src/components/navbar/links/links.jsx
@@ -1,20 +1,46 @@
-import Link from 'next/link'
+'use client'
+
+import { useState } from 'react'
 import style from './links.module.css'
 import NavLink from './navLink/navLink'
 
+const links = [
+  { title: 'Homepage', path: '/' },
+  { title: 'About', path: '/about' },
+  { title: 'Contact', path: '/contact' },
+  { title: 'Blog', path: '/blog' },
+]
+
 const Links = () => {
-  const links = [
-    { title: 'Homepage', path: '/' },
-    { title: 'About', path: '/about' },
-    { title: 'Contact', path: '/contact' },
-    { title: 'Blog', path: '/blog' },
-  ]
+  const [open, setOpen] = useState(false)
+
+  // temporary
+  const session = true
+  const isAdmin = true
 
   return (
-    <div className={style.links}>
-      {links.map((link, idx) => {
-        return <NavLink item={link} key={idx} />
-      })}
+    <div className={style.container}>
+      <div className={style.links}>
+        {links.map((link, idx) => {
+          return <NavLink item={link} key={idx} />
+        })}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
+            <button className={style.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: 'Login', path: '/login' }} />
+        )}
+      </div>
+      <button onClick={() => setOpen((prev) => !prev)}>Menu</button>
+      {open && (
+        <div className={style.mobileLinks}>
+          {links.map((link) => {
+            return <NavLink item={link} key={link.title} />
+          })}
+        </div>
+      )}
     </div>
   )
 }
