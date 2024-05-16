f37f9aaa45a385e366ffff0da9aa67f50f3fda1a
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-16 09:01:18 -0700

    lots of componments

---
+++ src/components/navbar/links/links.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+
+const Links = () => {
+  const links = [
+    { title: 'Homepage', path: '/' },
+    { title: 'About', path: '/about' },
+    { title: 'Contact', path: '/contact' },
+    { title: 'Blog', path: '/blog' },
+  ]
+
+  return (
+    <div>
+      {links.map((link, idx) => {
+        return (
+          <Link key={idx} href={link.path}>
+            {link.title}
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Links
