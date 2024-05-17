a149855c1c934d76c19fc7b4edecd7a32f6effd6
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-17 02:26:01 -0700

    Homepage design is pretty much done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,29 @@
+import styles from './home.module.css'
+import Image from 'next/image'
+
 const Home = () => {
-  return <div>Homepage</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque,
+          officiis nulla? Ex optio quos adipisci laboriosam eius iusto eveniet!
+          Aut ipsa iusto ipsum.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src='/brands.png' alt='' fill className={styles.brandsImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src='/hero.gif' alt='' fill className={styles.heroImg} />
+      </div>
+    </div>
+  )
 }
 
 export default Home
