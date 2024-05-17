07bade3f183c0edebbe8966cad41d630e3021b18
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-17 02:13:23 -0700

    basics of the next/image and basics of configs aswell

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,18 @@
+import Image from 'next/image'
+import styles from './about.module.css'
+
 const AboutPage = () => {
-  return <div>AboutPage</div>
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src='https://images.pexels.com/photos/23451993/pexels-photo-23451993/free-photo-of-black-and-white-photo-of-two-people-standing-in-a-circular-room.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
+          alt=''
+          fill
+        />
+      </div>
+    </div>
+  )
 }
 
 export default AboutPage
